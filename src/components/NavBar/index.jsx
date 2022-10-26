e3c348fb889249cba211d7d4abe94f81989fbb0c
Tadeo Cardelli <tadeocardelli@hotmail.com>
2022-10-26 14:37:42 -0300

    navbar-cartwidget

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import CartWidget from '../CartWidget'
+import './style.css'
+
+const NavBar = () => {
+    return (
+        <ul>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#news">Acerca de</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <CartWidget/>
+        </ul>
+    );
+};
+
+export default NavBar;
