c6fe21b65e59dd7f2e91b7f2aa19b4605987dd4c
Tadeo Cardelli <tadeocardelli@hotmail.com>
2022-10-27 14:26:27 -0300

    navbar-mejorada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting={"Bienvenido a mi pagina"}/>
+      <ItemListContainer greeting={"Hello coders!"}/>
     </div>
   );
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,21 +4,22 @@ import './style.css'
 
 const NavBar = () => {
     return (
-        <ul>
-            <li>
-                <a href="#home">Home</a>
-            </li>
-            <li>
-                <a href="#news">Acerca de</a>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-            <li>
-                <a href="#about">About</a>
-            </li>
-            <CartWidget/>
-        </ul>
+            <ul>
+                <li>
+                    <h3 className='titulo'>Doggie Pet</h3>
+                </li>
+
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#productos">Productos</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <CartWidget/>
+            </ul>
     );
 };
 
